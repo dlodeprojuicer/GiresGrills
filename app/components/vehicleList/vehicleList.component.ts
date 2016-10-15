dfef4fa9800796e34265daece191f738ade3537f
dlodeprojuicer <simom@hotmail.co.za>
2016-10-15 14:34:29 +0200

    _ filters.... w.i.p

--- app/components/vehicleList/vehicleList.component.ts
+++ app/components/vehicleList/vehicleList.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as _ from 'lodash';
 
 import { Vehicle } from '../../models/vehicles.model';
 import { VehicleProvider } from '../../providers/vehicle.provider';
@@ -11,22 +12,25 @@ import { VehicleProvider } from '../../providers/vehicle.provider';
 export class VehicleListComponent {
     vehicles: Vehicle[];
     originalVehicles: Vehicle[];
+    searchFilter:any;
 
     constructor(private vehicleProvider: VehicleProvider) {
         vehicleProvider.load().subscribe(vehicles => {
-        this.vehicles = vehicles;
-        console.log(vehicles)
+            console.log(vehicles);
+            this.vehicles = vehicles;
         })
     }
 
   search(searchEvent:any) {
-      console.log(searchEvent);
     let term = searchEvent.target.value
     // We will only perform the search if we have 3 or more characters
     if (term.trim() === '' || term.trim().length < 3) {
       // Load cached users
       this.vehicles = this.originalVehicles;
     } else {
+        console.log(searchEvent.target.value,this.vehicles);
+        this.searchFilter = _.filter(this.vehicles, _.iteratee({"make":searchEvent.target.value}));
+        console.log(this.searchFilter);
       // Get the searched users from github
       this.vehicleProvider.searchUsers(term).subscribe(vehicles => {
         this.vehicles = vehicles
